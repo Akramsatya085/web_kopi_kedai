f3f67fd341f18ab83bc6b11ee931bb6a9fa8c9b4
Akramsatya085 <akramsatya085@gmail.com>
2023-12-21 08:17:06 +0700

    web kopi kedai

---
+++ index.js
@@ -0,0 +1,7 @@
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+
+document.querySelector('#search-button').onclick = () => {
+    searchForm.classList.toggle('active');
+};
